1c5ee8e9ea1bae4a7aef28074dfacc00446481de
Vlad Brok <vlad.brok99@gmail.com>
2022-07-03 10:45:48 +0300

    (pokemon-app): Change img to Image

--- components/pokemon-app/Pokemon/Pokemon.js
+++ components/pokemon-app/Pokemon/Pokemon.js
@@ -1,11 +1,23 @@
 import styles from "./Pokemon.module.scss";
 import Button from "../../../components/pokemon-app/Button";
+import Image from "next/image";
+
+const IMAGE_SIZE = 160;
 
 export default function Pokemon({ name, imageUrl, onClick }) {
   return (
     <section className={styles.container}>
       <h2 className={styles.heading}>{name}</h2>
-      <img className={styles.image} src={imageUrl} alt={name} />
+      <div style={{ width: `${IMAGE_SIZE}px` }}>
+        <Image
+          layout="responsive"
+          width={IMAGE_SIZE}
+          height={IMAGE_SIZE}
+          objectFit="contain"
+          src={imageUrl}
+          alt={name}
+        />
+      </div>
       <Button onClick={onClick}>Prettier</Button>
     </section>
   );
