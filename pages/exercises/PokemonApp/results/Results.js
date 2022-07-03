68fe6fe897fa6e6f0316542c66f0e96a3d3b03fe
Vlad Brok <vlad.brok99@gmail.com>
2022-07-03 13:05:18 +0300

    (pokemon-app): Add pokemon list styling

--- pages/exercises/PokemonApp/results/Results.js
+++ pages/exercises/PokemonApp/results/Results.js
@@ -1,11 +1,24 @@
 import styles from "./Results.module.scss";
 import Layout from "../../../../components/pokemon-app/Layout";
+import Pokemon from "../../../../components/pokemon-app/Pokemon";
 
 export default function Results() {
   return (
     <Layout>
       <h1 className={styles.heading}>Results</h1>
-      <div className={styles.container}></div>
+      <ul className={styles.list}>
+        <li className={styles["list-item"]}>
+          <span className={styles.number}>1</span>
+          <div className={styles["pokemon-container"]}>
+            <Pokemon
+              imageSize={55}
+              name="joe"
+              imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg"
+            />
+          </div>
+          <span className={styles.percentage}>70%</span>
+        </li>
+      </ul>
     </Layout>
   );
 }
